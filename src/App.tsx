bb7e16e859d5ece630b6a5be1013331ad47a9cc6
simon0820s <simon0820s@gmail.com>
2023-06-29 12:02:33 -0500

    creating todo types and interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { useState } from 'react'
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false
+  }
+]
+
 const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos)
   return (
-    <div>Todos mdv</div>
+    <Todos todos={todos}/>
   )
 }
 
---
+++ src/components/Todos.tsx
@@ -0,0 +1,27 @@
+interface Todo {
+  id: string
+  title: string
+  completed: boolean
+}
+
+type ListOfTodos = Todo[]
+
+interface Props {
+  todos: ListOfTodos
+}
+
+const Todos: React.FC<Props> = ({ todos }) => {
+  return (
+    <ul>
+        {
+            todos.map(todo => (
+                <li key={todo.id}>
+                    {todo.title}
+                </li>
+            ))
+        }
+    </ul>
+  )
+}
+
+export default Todos
