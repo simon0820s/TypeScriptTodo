bd350fd1eb14027369e4b512465b91f8f5961c9f
simon0820s <simon0820s@gmail.com>
2023-07-05 16:42:24 -0500

    remove todos ready

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,15 +1,19 @@
 import { type Todo as TodoType } from '../types'
 
-type Props = TodoType
+interface Props extends TodoType {
+  onRemoveTodo: (id: string) => void
+}
 
-const Todo: React.FC<Props> = ({ id, title, completed }) => {
+const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo }) => {
   return (
     <div className="view">
         <input className="toggle" type="checkbox" checked={completed} onChange={() => {}}/>
         <label>{title}</label>
         <button
-         className="detroy"
-         onClick={() => {}}/>
+         className="destroy"
+         onClick={() => {
+           onRemoveTodo(id)
+         }}/>
     </div>
   )
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -3,15 +3,17 @@ import Todo from './Todo'
 
 interface Props {
   todos: ListOfTodos
+  onRemoveTodo: (id: string) => void
 }
 
-const Todos: React.FC<Props> = ({ todos }) => {
+const Todos: React.FC<Props> = ({ todos, onRemoveTodo }) => {
   return (
     <ul className='todo-list'>
       {
         todos.map(todo => (
           <li key={todo.id} className={`${todo.completed ? 'completed' : ''}`}>
-            <Todo key={todo.id} id={todo.id} title={todo.title} completed={todo.completed}/>
+            <Todo
+             key={todo.id} id={todo.id} title={todo.title} completed={todo.completed} onRemoveTodo={onRemoveTodo}/>
           </li>
         ))
       }
