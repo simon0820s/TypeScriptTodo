92f57fc97ec7ad939a304783ec8cc08d8e5d1a07
simon0820s <simon0820s@gmail.com>
2023-07-05 18:49:29 -0500

    complete todos ready

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,12 +2,25 @@ import { type TodoId, type Todo as TodoType } from '../types'
 
 interface Props extends TodoType {
   onRemoveTodo: ({ id }: TodoId) => void
+  onToggleCompleteTodo: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
 }
 
-const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo }) => {
+const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleCompleteTodo }) => {
+  const handleChangeCheckBox = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    onToggleCompleteTodo({
+      id,
+      completed: event.target.checked
+    })
+  }
+
   return (
     <div className="view">
-        <input className="toggle" type="checkbox" checked={completed} onChange={() => {}}/>
+        <input
+          className="toggle"
+          type="checkbox"
+          checked={completed}
+          onChange={handleChangeCheckBox}
+          />
         <label>{title}</label>
         <button
          className="destroy"
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,19 +1,20 @@
-import { type TodoId, type ListOfTodos } from '../types'
+import { type Todo as TodoType, type TodoId, type ListOfTodos } from '../types'
 import Todo from './Todo'
 
 interface Props {
   todos: ListOfTodos
-  onRemoveTodo: (id: TodoId) => void
+  onRemoveTodo: ({ id }: TodoId) => void
+  onToggleCompleteTodo: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void
 }
 
-const Todos: React.FC<Props> = ({ todos, onRemoveTodo }) => {
+const Todos: React.FC<Props> = ({ todos, onRemoveTodo, onToggleCompleteTodo }) => {
   return (
     <ul className='todo-list'>
       {
         todos.map(todo => (
           <li key={todo.id} className={`${todo.completed ? 'completed' : ''}`}>
             <Todo
-             key={todo.id} id={todo.id} title={todo.title} completed={todo.completed} onRemoveTodo={onRemoveTodo}/>
+             key={todo.id} id={todo.id} title={todo.title} completed={todo.completed} onRemoveTodo={onRemoveTodo} onToggleCompleteTodo={onToggleCompleteTodo}/>
           </li>
         ))
       }
